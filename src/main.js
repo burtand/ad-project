d82e2288ea7a50058380d8aa9b33727eb2691843
burtand <andrburtas@email.com>
2020-06-28 13:56:57 +0300

    Firebase installed and configured, store/user.js template created and imported to store/index.js and added to modules

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import * as firebase from 'firebase'
 
 Vue.use(vuetify)
 
@@ -12,5 +13,17 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBAzYpzlCypjoLG98zyKuVSk30N6Bwjvj8',
+      authDomain: 'burtand-ads.firebaseapp.com',
+      databaseURL: 'https://burtand-ads.firebaseio.com',
+      projectId: 'burtand-ads',
+      storageBucket: 'burtand-ads.appspot.com',
+      messagingSenderId: '363654882571',
+      appId: '1:363654882571:web:4744ca06d6738bfd4c6e0e',
+      measurementId: 'G-84C0D6R7TT'
+    })
+  }
 }).$mount('#app')
